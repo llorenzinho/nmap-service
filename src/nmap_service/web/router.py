e1f64b01dc2edd3d7335c04f933048b6bbc3c645
Lorenzo D'Agostino <lorenzo.dagostino.ref@gmail.com>
2026-03-09 23:07:27 +0100

    chore: scaffolded apis

---
+++ src/nmap_service/web/router.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, status
+
+router = APIRouter(
+   prefix="api/v1/scans",
+   tags=["v1"]
+)
+
+@router.post("")
+def run_job():
+    ...
+    
+@router.get("")
+def list_jobs():
+    ...
+    
+@router.get("{scan_id:str}/results")
+def get_job_detail(scan_id:str):
+    ...
